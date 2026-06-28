7bd0a413488103a2947d2a332466b4e159d393cd
SmileChaoLee <leechao1959@gmail.com>
2026-06-28 16:58:03 -0400

    Increased the Retrofit timeouts

--- app/src/main/java/com/smile/englishtutor/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/smile/englishtutor/retrofit/RetrofitClient.kt
@@ -10,9 +10,12 @@ object RetrofitClient {
     private const val CHAO_URL = "http://137.184.120.171:8001/"
     // private const val CHAO_URL = "http://10.0.2.2:8000/"    // Emulator for local
 
-
     fun getRetrofit(): Retrofit {
-        val retrofit = Client.getInstance(CHAO_URL)
+        val retrofit = Client.getInstance(
+            CHAO_URL,
+            connectTimeout = 60,
+            readTimeout = 60,
+            writeTimeout = 20)
         LogUtil.d(TAG, "getRetrofit.retrofit = $retrofit")
         return retrofit
     }
