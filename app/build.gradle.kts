c54187ebca59fece87ec27348fd8c145d379f0f7
SmileChaoLee <leechao1959@gmail.com>
2026-09-22 16:12:58 -0400

    Version code = 110

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.smile.englishtutor"
         minSdk = 24
         targetSdk = 37
-        versionCode = 109
-        versionName = "1.09"
+        versionCode = 110
+        versionName = "1.10"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
